292f7d8ba3b9a72691ed3672d6e09951454bf441
Kushagra Agrawal <kushjanhvi@gmail.com>
2023-04-28 01:16:32 +0530

    checkkkkk

--- pf.sh
+++ pf.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sleep 5s;kubectl port-forward client 3000:3000 & sleep 5s;kubectl port-forward server 5000:5000 & wait
+sleep 10s;kubectl port-forward client 3000:3000 & 
+sleep 10s;kubectl port-forward server 5000:5000 & 
+wait
