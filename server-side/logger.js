6051570d0694fb128f13abc649df2e1515c5287d
Kushagra Agrawal <kushjanhvi@gmail.com>
2023-05-10 23:37:29 +0530

    changes for logs automation

--- server-side/logger.js
+++ server-side/logger.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+const EST = require ('winston-elasticsearch');
 
 const logger = winston.createLogger({
   level: 'infoPointsTable',
@@ -11,9 +12,16 @@ const logger = winston.createLogger({
         winston.format.simple()
       )
     }),
+    new EST.ElasticsearchTransport ({
+      level: 'info',
+      index: 'logs',
+      clientOpts: {
+        node: 'http://localhost:9200/',
+      }
+    }),
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log', level: 'info' })
-  ]
+  ],
 });
 
 const setEndpointAsService = (req, res, next) => {
