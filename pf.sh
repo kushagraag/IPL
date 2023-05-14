b9beaece793bed1838e30a0b98d131b5895496d5
Kushagra Agrawal <kushjanhvi@gmail.com>
2023-05-14 09:58:36 +0530

    addd elk to minikube

--- pf.sh
+++ pf.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+sleep 10s;kubectl port-forward service/elasticsearch 9200:9200 & 
 sleep 10s;kubectl port-forward client 3000:3000 & 
 sleep 10s;kubectl port-forward server 5000:5000 & 
-sleep 10s;kubectl port-forward server 9200:9200 &
+sleep 30s;kubectl port-forward service/kibana 5601:5601 &
 wait
